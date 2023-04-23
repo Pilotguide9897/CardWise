341447e757e5eb91e7e251d672676ae61cacef42
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-23 19:38:57 -0300

    Added user routes (login, logout, create new user) and added authentication helper function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,9 +2,64 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 // POST User login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: {email: req.body.email} });
+
+        if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect login information, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+    } catch {
+    res.status(400).json(err);
+  }
+})
 
 // POST Create New User
+router.post('/new', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      name: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(newUser);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // POST Log out
+router.post('/logout', async (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+        res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,4 +3,13 @@ module.exports = {
     // replace with real helpers if needed.
     console.log("this is just a placeholder.");
   },
+
+  withAuth: (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  }
 };
+
