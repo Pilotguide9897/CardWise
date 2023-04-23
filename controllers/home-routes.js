3419741453fe06a27f958f1cfad3d3c7f9d17fd6
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-23 20:15:07 -0300

    Added entire route for homepage. It should show user decks when logged in and show the normal homepage when not logged in. I also added the beginning of other routes for the other GET routes.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,44 @@
 const router = require('express').Router();
 const { Deck, User } = require('../models');
+const { withAuth, checkAuth } = require('../utils/helpers');
 
 // GET Home page
-router.get('/', async (req, res) => {
-  res.render('homepage');
+router.get('/', checkAuth, async (req, res) => {
+  if (req.isAuthenticated ) {
+    try {
+      const decks = await Deck.findAll({
+        where: {
+          user_id: req.session.user_id
+        }
+      });
+
+      res.render("homepage", { userDeckData: 'decks' })
+    } catch (error) {
+      console.error('Error fetching deck data:', error);
+      res.status(500).send('Error fetching deck data');
+    }
+  } else {
+    res.render('homepage');
+  }
 });
 
 // GET New deck page
+router.get('/decks/new', withAuth, async (req, res) => {
+  res.render('');
+});
 
 // Get Update deck page
+router.get('/update/:id', withAuth, async (req, res) => {
+  res.render('');
+});
 
 // GET login/signup page
+router.get('/login', async (req, res) => {
+  res.render('');
+});
+
+router.get('/decks/:id', withAuth, async (req, res) => {
+  res.render('');
+});
 
 module.exports = router;
