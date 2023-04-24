a377067f23838d07657c8a0d46af4743e451bcf4
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-24 19:35:54 -0300

    removed placeholder from utils helpers.js file

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,9 @@
 module.exports = {
-  placeholder: () => {
-    // replace with real helpers if needed.
-    console.log("this is just a placeholder.");
-  },
-
   withAuth: (req, res, next) => {
     if (!req.session.logged_in) {
       res.redirect('/login');
     } else {
       next();
     }
-  }
+  },
 };
-
