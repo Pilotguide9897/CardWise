727b3fcabb59d6157e883f30ef48594775db4c1f
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-24 20:38:12 -0300

    Added get single deck and create deck endpoints

--- controllers/api/deck-routes.js
+++ controllers/api/deck-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Deck, Card } = require('../../models');
 
-// Getting all decks
+// Get all decks
 router.get('/', async (req, res) => {
   try {
     const deckData = await Deck.findAll({
@@ -29,8 +29,73 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Get a single deck
+router.get('/:id', async (req, res) => {
+  try {
+    const deckData = await Deck.findByPk(req.params.id, {
+      include: { model: Card },
+    });
+
+    if (!deckData) {
+      console.error({ message: 'A deck with that id does not exist' });
+      res.status(400).json({ message: 'A deck with that id does not exist' });
+      return;
+    }
+    const deck = deckData.get({ plain: true });
+
+    res.json(deck);
+  } catch (err) {
+    console.error({
+      message: 'there was a problem finding the deck',
+      error: err,
+    });
+  }
+});
+
 // Updating a deck
+// Update deck title/description
 // batch update cards
+
+// New Deck
+router.post('/', async (req, res) => {
+  try {
+    const deckData = await Deck.create(
+      {
+        user_id: req.body.user_id,
+        name: req.body.name,
+        new_cards_per_day: req.body.new_cards_per_day,
+      },
+      {
+        include: Card,
+      }
+    );
+
+    const newCardData = req.body.cards.map((card) => {
+      return {
+        ...card,
+        deck_id: deckData.id,
+        interval: 0,
+        repetition: 0,
+        e_factor: 2.5,
+      };
+    });
+
+    await Card.bulkCreate(newCardData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    res.json(deckData);
+  } catch (err) {
+    console.error({
+      message: 'there was a problem creating the deck',
+      error: err,
+    });
+    res
+      .status(500)
+      .json({ message: 'there was a problem creating the deck', error: err });
+  }
+});
 
 // deleting a deck
 router.delete('/:id', async (req, res) => {
@@ -60,9 +125,5 @@ router.delete('/:id', async (req, res) => {
       .json({ message: 'There was an error deleting the Deck', error: err });
   }
 });
-
-// Getting a single deck.
-
-// Updating reviewed cards.
 
 module.exports = router;
