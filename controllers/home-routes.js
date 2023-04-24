d20cb43b31ded84e25a679cc48604c85af1fb93f
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-24 20:15:36 -0300

    added dashboard route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,21 +3,48 @@ const { Deck, User, Card } = require('../models');
 const { withAuth, checkAuth } = require('../utils/helpers');
 
 // GET Home page
-router.get('/', checkAuth, async (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage');
+  } catch (error) {
+    console.error('Error accessing homepage:', error);
+    res
+      .status(500)
+      .json({
+        message: 'There was an error reaching the homepage.',
+        error: err,
+      });
+  }
+});
+
+// GET Dashboard
+router.get('/dashboard', withAuth, async (req, res) => {
   if (req.session.logged_in) {
     try {
-      const decks = await Deck.findAll({
-        where: {
-          user_id: req.session.user_id,
-        },
+      const deckData = await Deck.findAll({
+        include: { model: Card },
       });
-      res.render('homepage', {
-        userDeckData: decks,
-        logged_in: req.session.logged_in,
+
+      if (!deckData) {
+        res.status(400).json({ message: 'Unable to locate decks.' });
+        return;
+      }
+
+      const decks = deckData.map((deck) => {
+        return deck.get({ plain: true });
       });
-    } catch (error) {
-      console.error('Error fetching deck data:', error);
-      res.status(500).send('Error fetching deck data');
+
+      res.render('dashboard', {
+        deckData: decks,
+      });
+    } catch (err) {
+      console.error({
+        message: 'There was an error getting all decks.',
+        error: err,
+      });
+      res
+        .status(500)
+        .json({ message: 'There was an error getting all decks.', error: err });
     }
   } else {
     res.render('homepage');
@@ -33,7 +60,7 @@ router.get('/decks/new', withAuth, async (req, res) => {
       });
     } catch (error) {
       console.error('Error rendering your page:', error);
-      res.status(500).send('Error fetching /decks/new');
+      res.status(500).json({ message: 'Error fetching /decks/new' });
     }
   } else {
     res.render('homepage');
@@ -48,7 +75,7 @@ router.get('/updateDeck/:id', withAuth, async (req, res) => {
         include: [Card],
       });
       if (!deckToUpdate) {
-        res.status(404).send('Deck not found');
+        res.status(404).json({ message: 'Deck not found' });
         return;
       }
       res.render('updateDeck', {
@@ -58,7 +85,7 @@ router.get('/updateDeck/:id', withAuth, async (req, res) => {
       });
     } catch (error) {
       console.error('Error rendering your page:', error);
-      res.status(500).send('Error fetching /decks/:id');
+      res.status(500).json({ message: 'Error rendering /updateDecks/:id' });
     }
   } else {
     res.render('homepage');
@@ -71,13 +98,33 @@ router.get('/login', async (req, res) => {
     res.render('login');
   } catch (error) {
     console.error('Error rendering your page:', error);
-    res.status(500).send('Error rendering login page');
+    res.status(500).json({ message: 'Error rendering login page' });
   }
 });
 
 // GET individual decks
 router.get('/decks/:id', withAuth, async (req, res) => {
-  res.render('deckView', { logged_in: req.session.logged_in });
+  if (req.session.logged_in) {
+    try {
+      const deckToReview = await Deck.findByPk(req.params.id, {
+        include: [Card],
+      });
+      if (!deckToUpdate) {
+        res.status(404).json({ message: 'Deck not found' });
+        return;
+      }
+      res.render('reviewDeck', {
+        deckData: deckToReview,
+        deck: deckToReview.Cards,
+        logged_in: req.session.logged_in,
+      });
+    } catch (error) {
+      console.error('Error rendering your page:', error);
+      res.status(500).json({ message: 'Error rendering /decks/:id' });
+    }
+  } else {
+    res.render('homepage');
+  }
 });
 
 // Catch all route for redirecting request to incorrect endpoints.
@@ -86,3 +133,34 @@ router.get('*', (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+// I just had this written back when I thought we would make the homepage present all of our decks if we were logged in, rather than having the dashboard. I am leaving it in the code for now, but we can take it out when we know for certain that we will not need it!
+// router.get('/', checkAuth, async (req, res) => {
+//   if (req.session.logged_in) {
+//     try {
+//       const decks = await Deck.findAll({
+//         where: {
+//           user_id: req.session.user_id,
+//         },
+//       });
+//       res.render('homepage', {
+//         userDeckData: decks,
+//         logged_in: req.session.logged_in,
+//       });
+//     } catch (error) {
+//       console.error('Error fetching deck data:', error);
+//       res.status(500).send('Error fetching deck data');
+//     }
+//   } else {
+//     res.render('homepage');
+//   }
+// });
