253802e628fa4073639d02b1f96e775a2d5eb4b6
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-24 19:04:13 -0300

    reduced number of new cards per day in deck seed data

--- seeds/deckData.js
+++ seeds/deckData.js
@@ -1,14 +1,14 @@
 const deckData = [
-  { user_id: 1, name: 'Math', new_cards_per_day: 10 },
-  { user_id: 2, name: 'Science', new_cards_per_day: 7 },
-  { user_id: 3, name: 'English', new_cards_per_day: 5 },
-  { user_id: 4, name: 'History', new_cards_per_day: 8 },
-  { user_id: 5, name: 'Geography', new_cards_per_day: 12 },
-  { user_id: 1, name: 'Art', new_cards_per_day: 6 },
-  { user_id: 2, name: 'Music', new_cards_per_day: 9 },
-  { user_id: 3, name: 'Physical Education', new_cards_per_day: 11 },
-  { user_id: 4, name: 'Computing', new_cards_per_day: 15 },
-  { user_id: 5, name: 'Biology', new_cards_per_day: 13 },
+  { user_id: 1, name: 'Math', new_cards_per_day: 3 },
+  { user_id: 2, name: 'Science', new_cards_per_day: 2 },
+  { user_id: 3, name: 'English', new_cards_per_day: 1 },
+  { user_id: 4, name: 'History', new_cards_per_day: 2 },
+  { user_id: 5, name: 'Geography', new_cards_per_day: 3 },
+  { user_id: 1, name: 'Art', new_cards_per_day: 2 },
+  { user_id: 2, name: 'Music', new_cards_per_day: 3 },
+  { user_id: 3, name: 'Physical Education', new_cards_per_day: 3 },
+  { user_id: 4, name: 'Computing', new_cards_per_day: 4 },
+  { user_id: 5, name: 'Biology', new_cards_per_day: 5 },
 ];
 
 module.exports = deckData;
