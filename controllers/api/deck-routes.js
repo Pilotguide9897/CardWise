db1d0d904db9978c9a247bce50e131704445339e
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-24 19:05:54 -0300

    completed get all deck api end point, removed email property from user data model

--- controllers/api/deck-routes.js
+++ controllers/api/deck-routes.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
-const { Deck } = require('../../models');
+const { Deck, Card } = require('../../models');
 
 // Getting all decks
+router.get('/', async (req, res) => {
+  try {
+    const deckData = await Deck.findAll({
+      include: { model: Card },
+    });
+
+    if (!deckData) {
+      res.status(400).json({ message: 'Unable to locate decks.' });
+      return;
+    }
+
+    const decks = deckData.map((deck) => {
+      return deck.get({ plain: true });
+    });
+
+    res.json(decks);
+  } catch (err) {
+    console.error({
+      message: 'There was an error getting all decks.',
+      error: err,
+    });
+    res
+      .status(500)
+      .json({ message: 'There was an error getting all decks.', error: err });
+  }
+});
 
 // Updating a deck
 
