c938fb0fa6541173309c898ee6c7fdeffb3e5df0
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-24 21:02:33 -0300

    added client-side logic for handling login, logout, and signup.

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,48 @@
 // Client side logic for processing login and signup forms.
+const loginHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signUpHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/new', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const loginForm = document.getElementById('loginFormId'); //need to add correct id.
+loginForm.addEventListener('submit', loginHandler);
+
+const signUpForm = document.getElementById('signUpFormID'); //need to add correct id.
+signUpForm.addEventListener('submit', signUpHandler);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,16 @@
 // Client side logic for processing logout.
+const logoutHandler = async () => {
+  const response = await fetch('/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+const logoutForm = document.getElementById('logoutButtonId'); //need to add correct id.
+logoutForm.addEventListener('click', logoutHandler);
