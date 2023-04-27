ca76dee369519accfd5cdd64fb99244c524a1fa4
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-27 19:09:17 -0300

    Added deck update endpoint

--- controllers/api/deck-routes.js
+++ controllers/api/deck-routes.js
@@ -5,6 +5,9 @@ const { Deck, Card } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const deckData = await Deck.findAll({
+      attributes: {
+        exclude: ['user_id', 'userId', 'createdAt', 'updatedAt'],
+      },
       include: { model: Card },
     });
 
@@ -33,7 +36,24 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const deckData = await Deck.findByPk(req.params.id, {
-      include: { model: Card },
+      attributes: {
+        exclude: ['user_id', 'userId', 'createdAt', 'updatedAt'],
+      },
+      include: {
+        model: Card,
+        attributes: {
+          exclude: [
+            'deck_id',
+            'is_queued',
+            'interval',
+            'repetition',
+            'e_factor',
+            'createdAt',
+            'updatedAt',
+            'deckId',
+          ],
+        },
+      },
     });
 
     if (!deckData) {
@@ -52,23 +72,100 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// Updating a deck
-// Update deck title/description
-// batch update cards
-
-// New Deck
-router.post('/', async (req, res) => {
+// Updating a deck and it's cards.
+router.put('/:id', async (req, res) => {
   try {
-    const deckData = await Deck.create(
+    // update deck properties.
+    const deckData = await Deck.update(
       {
         user_id: req.body.user_id,
         name: req.body.name,
         new_cards_per_day: req.body.new_cards_per_day,
       },
       {
-        include: Card,
+        where: {
+          id: req.params.id,
+        },
       }
     );
+
+    // get all cards associated to with this deck from the db.
+    let currentCards = await Card.findAll({
+      where: { deck_id: req.params.id },
+    });
+    currentCards = currentCards.map((card) => card.get({ plain: true }));
+
+    // Make a list of cards that exist in currentCards that
+    // but are missing from req.body.cards.
+    const cardsToDelete = currentCards.filter(
+      (card) => !req.body.cards.some((newCard) => newCard.id === card.id)
+    );
+    // create a list of the card ids to be used for bulk destoy.
+    const cardsToDeleteIds = cardsToDelete.map((card) => card.id);
+    if (cardsToDeleteIds.length > 0) {
+      await Card.destroy({ where: { id: cardsToDeleteIds } });
+    }
+
+    let cardQueue = [];
+    req.body.cards.forEach((newCard) => {
+      // find card in both arrays with same id.
+      let result = currentCards.find(
+        (currentCard) => currentCard.id === newCard.id
+      );
+      let cardToProcess;
+      if (result) {
+        // if the new card front or back has been changed, add the card to the queue.
+        if (newCard.front !== result.front || newCard.back !== result.back) {
+          cardToProcess = newCard;
+        }
+      } else {
+        // if there is no result, it's because this card is brand new.
+        cardToProcess = newCard;
+      }
+
+      if (cardToProcess) {
+        cardQueue.push({
+          ...cardToProcess,
+          deck_id: req.params.id,
+          interval: 0,
+          repetition: 0,
+          e_factor: 2.5,
+        });
+      }
+    });
+
+    await Card.bulkCreate(cardQueue, {
+      updateOnDuplicate: [
+        'front',
+        'back',
+        'interval',
+        'repetition',
+        'e_factor',
+      ],
+      individualHooks: true,
+      returning: true,
+    });
+
+    res.json({ message: 'Deck updated successfully.', data: deckData });
+  } catch (err) {
+    console.error({
+      message: 'There was an error updating the deck.',
+      error: err,
+    });
+    res
+      .status(500)
+      .json({ message: 'There was an error updating the deck.', error: err });
+  }
+});
+
+// New Deck
+router.post('/', async (req, res) => {
+  try {
+    const deckData = await Deck.create({
+      user_id: req.body.user_id,
+      name: req.body.name,
+      new_cards_per_day: req.body.new_cards_per_day,
+    });
 
     const newCardData = req.body.cards.map((card) => {
       return {
