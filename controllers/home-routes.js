bf80e294bd644080d624fdf99ba32e2e2cf34dcb
Victoria Benoit <vrose834@gmail.com>
2023-04-27 14:55:10 -0300

    added a box shadow to the login/signup and centered it a bit different

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,144 +10,14 @@ router.get('/login', async (req, res) => {
 router.get('/finish', async (req, res) => {
   res.render('finish');
 });
-router.get('/decks', async (req, res) => {
-  res.render('decks');
+router.get('/dashboard', async (req, res) => {
+  res.render('dashboard');
 });
 router.get('/createdeck', async(req, res) => {
   res.render('createdeck');
 });
 
 
-// GET Home page
-router.get('/', async (req, res) => {
-  try {
-    res.render('homepage');
-  } catch (error) {
-    console.error('Error accessing homepage:', error);
-    res
-      .status(500)
-      .json({
-        message: 'There was an error reaching the homepage.',
-        error: err,
-      });
-  }
-});
-
-// GET Dashboard //
-router.get('/dashboard', withAuth, async (req, res) => {
-  if (req.session.logged_in) {
-    try {
-      const deckData = await Deck.findAll({
-        include: { model: Card },
-      });
-
-      if (!deckData) {
-        res.status(400).json({ message: 'Unable to locate decks.' });
-        return;
-      }
-
-      const decks = deckData.map((deck) => {
-        return deck.get({ plain: true });
-      });
-
-      res.render('dashboard', {
-        deckData: decks,
-      });
-    } catch (err) {
-      console.error({
-        message: 'There was an error getting all decks.',
-        error: err,
-      });
-      res
-        .status(500)
-        .json({ message: 'There was an error getting all decks.', error: err });
-    }
-  } else {
-    res.render('homepage');
-  }
-});
-
-
-// GET New deck page
-router.get('/decks/new', withAuth, async (req, res) => {
-  if (req.session.logged_in) {
-    try {
-      res.render('newDeck', {
-        logged_in: req.session.logged_in,
-      });
-    } catch (error) {
-      console.error('Error rendering your page:', error);
-      res.status(500).json({ message: 'Error fetching /decks/new' });
-    }
-  } else {
-    res.render('homepage');
-  }
-});
-
-// Get Update deck page
-router.get('/updateDeck/:id', withAuth, async (req, res) => {
-  if (req.session.logged_in) {
-    try {
-      const deckToUpdate = await Deck.findByPk(req.params.id, {
-        include: [Card],
-      });
-      if (!deckToUpdate) {
-        res.status(404).json({ message: 'Deck not found' });
-        return;
-      }
-      res.render('updateDeck', {
-        deckData: deckToUpdate,
-        deck: deckToUpdate.Cards,
-        logged_in: req.session.logged_in,
-      });
-    } catch (error) {
-      console.error('Error rendering your page:', error);
-      res.status(500).json({ message: 'Error rendering /updateDecks/:id' });
-    }
-  } else {
-    res.render('homepage');
-  }
-});
-
-// GET login/signup page
-router.get('/login', async (req, res) => {
-  try {
-    res.render('login');
-  } catch (error) {
-    console.error('Error rendering your page:', error);
-    res.status(500).json({ message: 'Error rendering login page' });
-  }
-});
-
-// GET individual decks
-router.get('/decks/:id', withAuth, async (req, res) => {
-  if (req.session.logged_in) {
-    try {
-      const deckToReview = await Deck.findByPk(req.params.id, {
-        include: [Card],
-      });
-      if (!deckToUpdate) {
-        res.status(404).json({ message: 'Deck not found' });
-        return;
-      }
-      res.render('reviewDeck', {
-        deckData: deckToReview,
-        deck: deckToReview.Cards,
-        logged_in: req.session.logged_in,
-      });
-    } catch (error) {
-      console.error('Error rendering your page:', error);
-      res.status(500).json({ message: 'Error rendering /decks/:id' });
-    }
-  } else {
-    res.render('homepage');
-  }
-});
-
-// Catch all route for redirecting request to incorrect endpoints.
-router.get('*', (req, res) => {
-  res.redirect('/');
-});
 
 module.exports = router;
 
