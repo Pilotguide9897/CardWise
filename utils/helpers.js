a83cf20faf5d340281a341a737e4042e7ad6c3e9
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-28 16:09:07 -0300

    almost have the helper function and route completed

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,8 @@
 // Import the required libraries and modules
 const Card = require('../models/Card');
 const dayjs = require('dayjs');
-const { supermemo } = require('supermemo'); // I guess that I 
+const { supermemo } = require('supermemo');
+const getCardData = require('../controllers/api/deck-routes')
 
 module.exports = {
   withAuth: (req, res, next) => {
@@ -24,4 +25,14 @@ module.exports = {
 
     return { ...flashcard, interval, repetition, efactor, dueDate };
   },
+
+  // Helper function to implement the mathpix library.
+  renderCardWithMathpix: async (cards) => {
+    const cardContent = await getCardData (cards);
+    const renderedHTML = renderMarkdown(cardContent);
+    // Replace the content of the card element with the rendered html
+    const cardElement = document.getElementById('cardbody'); // Placeholder id
+    cardElement.innerHTML = renderedHTML;
+  },
+
 };
