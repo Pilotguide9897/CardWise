e0bec208e58ecdb046c5bb55b9e0ae68ab955862
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-29 20:56:43 -0300

    created test routes for testing and utilities.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const deckRoutes = require('./deck-routes');
 const userRoutes = require('./user-routes');
+const testRoutes = require('./test-routes');
 
 router.use('/decks', deckRoutes);
 router.use('/users', userRoutes);
+router.use('/test', testRoutes);
 
 module.exports = router;
