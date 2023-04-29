cb52dc7cf67b300cb88e506efed60d30e024e3a6
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-29 20:56:43 -0300

    created test routes for testing and utilities.

---
+++ controllers/api/test-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const updateDeckQueues = require('../../utils/update-queues');
+// const { practice } = require('../../utils/helpers');
+
+// Utility end point, used to queue cards for testing.
+router.get('/queue-decks', (req, res) => {
+  updateDeckQueues();
+  res.json('It probably worked, check the node console.');
+});
+
+module.exports = router;
