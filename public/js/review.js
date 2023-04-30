ce95901d677787c706970384c291fc2a874b1651
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-30 15:03:02 -0300

    forgot to send data to db

--- public/js/review.js
+++ public/js/review.js
@@ -1,47 +1,12 @@
-const { updateSupermemoInfo } = require("../../utils/helpers");
-
-  let isFrontSideDisplayed = true;
-
-  const frontSide = document.getElementById('frontSide');
-  const backSide = document.getElementById('backSide');
-  const answerBtn = document.getElementById('answerBtn');
-  const nextBtn = document.getElementById('nextBtn');
-
-  const cardsUpForReview = {{{cardsUpForReview}}}; // Use server data
-
-  let currentCardIndex = 0;
-  frontSide.innerHTML = cardsUpForReview[currentCardIndex].front;
-  backSide.innerHTML = cardsUpForReview[currentCardIndex].back;
-
-  answerBtn.addEventListener('click', () => {
-    isFrontSideDisplayed = !isFrontSideDisplayed;
-    frontSide.style.display = isFrontSideDisplayed ? 'block' : 'none';
-    backSide.style.display = isFrontSideDisplayed ? 'none' : 'block';
-  });
-
-  nextBtn.addEventListener('click', () => {
-    const grade = document.querySelector('input[name="supermemo"]:checked').value;
-    let card = cardsUpForReview[currentCardIndex];
-    isFrontSideDisplayed = true;
-    frontSide.style.display = 'block';
-    backSide.style.display = 'none';
-    const updatedCard = updateSupermemoInfo(card, grade);
-    await updateCard(updatedCard);
-
-    if (currentCardIndex >= cardsUpForReview.length) {
-    alert('You have finished reviewing all the cards for toady');
-    document.location.replace('/dashboard');
-  }
-
-    frontSide.innerHTML = cardsUpForReview[currentCardIndex].front;
-    backSide.innerHTML = cardsUpForReview[currentCardIndex].back;
-  });
+const { updateSupermemoInfo } = require('../../utils/helpers');
 
 // Send the data to the db to update supermemo information
-  async function updateCard(updatedCard) {
+async function updateCard(updatedCard) {
   try {
-    const response = await fetch(`/api/cards/${card.id}`, { // I need to get the correct route...
+    const response = await fetch(`/api/cards/${card.id}`, {
+      // I need to get the correct route...
       method: 'PUT',
+      body: JSON.stringify({ updatedCard }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -57,3 +22,38 @@ const { updateSupermemoInfo } = require("../../utils/helpers");
     console.error('Error updating card:', error);
   }
 }
+
+let isFrontSideDisplayed = true;
+
+const frontSide = document.getElementById('frontSide');
+const backSide = document.getElementById('backSide');
+const answerBtn = document.getElementById('answerBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentCardIndex = 0;
+frontSide.innerHTML = cardsUpForReview[currentCardIndex].front;
+backSide.innerHTML = cardsUpForReview[currentCardIndex].back;
+
+answerBtn.addEventListener('click', () => {
+  isFrontSideDisplayed = !isFrontSideDisplayed;
+  frontSide.style.display = isFrontSideDisplayed ? 'block' : 'none';
+  backSide.style.display = isFrontSideDisplayed ? 'none' : 'block';
+});
+
+nextBtn.addEventListener('click', () => {
+  const grade = document.querySelector('input[name="supermemo"]:checked').value;
+  let card = cardsUpForReview[currentCardIndex];
+  isFrontSideDisplayed = true;
+  frontSide.style.display = 'block';
+  backSide.style.display = 'none';
+  const updatedCard = updateSupermemoInfo(card, grade);
+  updateCard(updatedCard);
+
+  if (currentCardIndex >= cardsUpForReview.length) {
+    alert('You have finished reviewing all the cards for toady');
+    document.location.replace('/dashboard');
+  }
+
+  frontSide.innerHTML = cardsUpForReview[currentCardIndex].front;
+  backSide.innerHTML = cardsUpForReview[currentCardIndex].back;
+});
