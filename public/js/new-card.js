442913623d4971da2ceef89b9ef19b889114f77e
Victoria Benoit <vrose834@gmail.com>
2023-05-02 10:53:13 -0300

    more

--- public/js/new-card.js
+++ public/js/new-card.js
@@ -1,3 +1,4 @@
+// Client side logic for card creation
 const addCardButton = document.getElementById('add-cue-card');
 const createButton = document.getElementById('create');
 const cueCardContainer = document.getElementById('cue-card-container');
@@ -14,10 +15,10 @@ function addCard() {
   cueCardForm.setAttribute('class', 'pure-u-1 cardForm');
 
   cueCardForm.innerHTML = `
-    <label for="front-${cueCardCount}">Term: </label>
-    <input type="text" id="front-${cueCardCount}" name="front-${cueCardCount}">
-    <label for="back-${cueCardCount}">Definition: </label>
-    <input type="text" id="back-${cueCardCount}" name="back-${cueCardCount}">
+    <label for="front-${cueCardCount-5}">Term: </label>
+    <input type="text" id="front-${cueCardCount-5}" name="front-${cueCardCount-5}">
+    <label for="back-${cueCardCount-5}">Definition: </label>
+    <input type="text" id="back-${cueCardCount-5}" name="back-${cueCardCount-5}">
   `;
   cueDiv.appendChild(cueCardForm);
   cueCardContainer.appendChild(cueDiv);
@@ -29,10 +30,12 @@ async function createCueCards() {
 
   for (let i = 1; i <= cueCardCount; i++) {
     const front = document.querySelector(`#front-${i}`).value.trim();
+    console.log(front);
     const back = document.querySelector(`#back-${i}`).value.trim();
+    console.log(back);
 
     if (front && back) {
-      cueCardData.push({ front, back});
+      cueCardData.push({ front, back });
     } else {
       alert(`Please fill in all fields for cue card ${i}.`);
       return;
@@ -42,7 +45,8 @@ async function createCueCards() {
   const description = document.querySelector('#description').value;
   const new_cards_per_day = document.querySelector('#cardsPerDay').value;
 
-  const response = await fetch('/api/decks', {
+  const response = await fetch('/api/decks/cuecards', {
+    // We may need to modify the last parameter of the route to make it reflect the actual route that we will be using for creating a new deck.
     method: 'POST',
     body: JSON.stringify({name, description, new_cards_per_day, cueCardData}),
     headers: { 'Content-Type': 'application/json' },
@@ -55,8 +59,13 @@ async function createCueCards() {
   }
 }
 
-addCardButton.addEventListener('click', addCard);
-createButton.addEventListener('click', createCueCards);
+if (addCardButton) {
+  addCardButton.addEventListener('click', addCard);
+}
+
+if (createButton) {
+  createButton.addEventListener('click', createCueCards);
+}
 
 // Create initial cue cards
 const initialCueCardCount = 4;
