01b9eb8069a6d7e1632baf765c653c7422297f2d
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-05-02 14:58:57 -0300

    create deck is working now

--- public/js/new-card.js
+++ public/js/new-card.js
@@ -21,10 +21,10 @@ function addCard() {
   cueCardForm.setAttribute('class', 'pure-u-1 cardForm');
 
   cueCardForm.innerHTML = `
-    <label for="front-${cueCardCount-5}">Term: </label>
-    <input type="text" id="front-${cueCardCount-5}" name="front-${cueCardCount-5}">
-    <label for="back-${cueCardCount-5}">Definition: </label>
-    <input type="text" id="back-${cueCardCount-5}" name="back-${cueCardCount-5}">
+    <label for="front-${cueCardCount}">Term: </label>
+    <input type="text" id="front-${cueCardCount}" name="front-${cueCardCount}">
+    <label for="back-${cueCardCount}">Definition: </label>
+    <input type="text" id="back-${cueCardCount}" name="back-${cueCardCount}">
     <button class="delCard">Delete</button>
   `;
   cueDiv.appendChild(cueCardForm);
@@ -41,15 +41,12 @@ async function createCueCards() {
 
   for (let i = 1; i <= actualCueCardCount; i++) {
     const front = document.querySelector(`#front-${i}`).value.trim();
-    console.log(front);
     const back = document.querySelector(`#back-${i}`).value.trim();
-    console.log(back);
 
     if (front && back) {
       cueCardData.push({ front, back });
-      console.log(cueCardData);
     } else {
-      alert(`Please fill in all fields for cue card ${i}.`);
+      alert(`Please fill in all fields for each of your cards.`);
       return;
     }
   }
@@ -80,7 +77,7 @@ if (createButton) {
 }
 
 // Create initial cue cards
-const initialCueCardCount = 4;
+const initialCueCardCount = 5;
 for (let i = 0; i < initialCueCardCount; i++) {
   addCard();
 }
