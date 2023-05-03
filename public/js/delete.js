fc52a53f3ac7b96a4dd1f4c313c689569eff804d
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-05-03 12:43:53 -0300

    revised delete script to use event delegation

--- public/js/delete.js
+++ public/js/delete.js
@@ -21,13 +21,14 @@ const deleteDeck = async (event) => {
   }
 };
 
-document.querySelector('#deleteBtn').addEventListener('click', deleteDeck);
+const deleteBtn = document.querySelector('#deleteBtn');
+if (deleteBtn) {
+  deleteBtn.addEventListener('click', deleteDeck);
+}
 
 // Delete an individual card
-
-
-
 function removeCard(event) {
+  console.log('remove card started');
   // Show a confirmation dialog
   const userConfirmed = confirm('Are you sure you want to delete this card?');
 
@@ -35,10 +36,33 @@ function removeCard(event) {
   if (userConfirmed) {
     // Find the closest parent element with the class 'cardDiv' and remove it
     const cardDiv = event.target.closest('.pure-g');
+    console.log(event.target);
+    console.log(cardDiv);
     if (cardDiv) {
       cardDiv.remove();
     }
   }
 }
 
-document.querySelector('.delCard').addEventListener('click', removeCard);
+const updateCardDiv = document.getElementById('updateCardsDiv');
+if (updateCardDiv) {
+  updateCardDiv.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains('delCard')) {
+      console.log('update eventd delete correct button');
+      removeCard(event);
+    }
+  });
+}
+
+const newCardDiv = document.getElementById('cue-card-container');
+if (newCardDiv) {
+  newCardDiv.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains('delCard')) {
+      removeCard(event);
+    }
+  });
+}
