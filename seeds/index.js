56344fb2821aedd5dc5cd3a02c8a4455ac3ce384
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-05-03 15:05:47 -0300

    changed seed data and funneled it all to Sal

--- seeds/index.js
+++ seeds/index.js
@@ -5,16 +5,16 @@ const userData = require('./userData');
 const deckData = require('./deckData');
 const cardData = require('./cardData');
 
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
-};
-
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  await User.bulkCreate([userData[0]], {
     individualHooks: true,
     returning: true,
+  });
+
+  deckData.forEach((deck) => {
+    deck.user_id = 1;
   });
 
   await Deck.bulkCreate(deckData, {
@@ -23,13 +23,7 @@ const seedDatabase = async () => {
   });
 
   const newCardData = cardData.map((card) => {
-    return {
-      ...card,
-      is_queued: false,
-      interval: getRandomInt(5),
-      repetition: 0,
-      efactor: 2.5,
-    };
+    return { ...card, interval: 0, repetition: 0, efactor: 2.5 };
   });
 
   await Card.bulkCreate(newCardData, {
